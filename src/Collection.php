a888b2600793aee21b5369ffbd0d3a2ab38e569d
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-12-08 17:36:51 +0000

    Added outline of collection class

---
+++ src/Collection.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PublishingKit\Collection;
+
+use ArrayAccess;
+use Countable;
+use IteratorAggregate;
+use ArrayIterator;
+
+class Collection implements ArrayAccess, Countable, IteratorAggregate
+{
+    /**
+     * @var array
+     */
+    private $items;
+
+    public function __construct($items)
+    {
+        $this->items = $items;
+    }
+
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->items);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->items[$offset];
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if ($offset === null) {
+            $this->items[] = $value;
+        } else {
+            $this->items[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->items[$offset]);
+    }
+
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    public function getIterator()
+    {
+        return new ArrayIterator($this->items);
+    }
+}
